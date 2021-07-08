2e80820e12c6b6d2a898708d20b9dd5a3d510afa
Nuvaran <rng.Nuvi777@gmail.com>
2021-07-08 21:08:01 +0200

    First commit

---
+++ App.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const connectDB = require('./config/db');
+require("dotenv").config({ path: "./config/.env" });
+
+var cors = require('cors');
+
+//routes
+const books = require('./routes/api/Book');
+
+const app = express();
+
+// Connect Database
+connectDB();
+
+//cors
+app.use(cors({ origin: true, credentials: true }));
+
+//Init Middleware
+app.use(express.json({ extended: false }));
+
+app.get('/', (req, res) => res.send('Hello world!'));
+
+//use Routes
+app.use('./api/books', books);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
